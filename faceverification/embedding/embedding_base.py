53ec888c6ff14e7678ce13b8c0ad1270c3124715
Igor Avdonin <igor.avdonin@idrnd.net>
2022-05-08 21:48:35 +0500

    refactoring: Change get_embedding method return type

--- faceverification/embedding/embedding_base.py
+++ faceverification/embedding/embedding_base.py
@@ -1,12 +1,12 @@
 """ Base class for all face embeddings models """
 
 from abc import ABC, abstractmethod
-from typing import Collection
+from typing import Tuple
 
 
 class FaceEmbeddingModel(ABC):
     """Base class for models that calculates face embedding"""
 
     @abstractmethod
-    def get_embedding(self, face_image: bytes, **kwargs) -> Collection[float]:
+    def get_embedding(self, face_image: bytes, **kwargs) -> Tuple[float]:
         """Returns face image emgedding"""
