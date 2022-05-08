9a14e560daa822adcd0d274b50a8f595c865f9ce
Igor Avdonin <igor.avdonin@idrnd.net>
2022-05-09 01:15:33 +0500

    feat: Add default verification pipeline

--- faceverification/verification/__init__.py
+++ faceverification/verification/__init__.py
@@ -1,1 +1,23 @@
 """ Module with face verification tools """
+
+from faceverification.detection import MTCNNDetector
+from faceverification.embedding import InceptionResnetV1EmbeddingModel
+from faceverification.distance.metrics import EuclideanDistance
+from .verificator_base import FaceVerificator, CustomClassicVerificator
+
+__all__ = [
+    "FaceVerificator",
+    "CustomClassicVerificator",
+    "get_default_face_verificator",
+]
+
+
+def get_default_face_verificator(device: str = "cpu") -> FaceVerificator:
+    return CustomClassicVerificator(
+        face_detector=MTCNNDetector(device=device),
+        face_embedding_model=InceptionResnetV1EmbeddingModel(device=device),
+        distance_metric=EuclideanDistance(l2_normalization=True),
+        detector_threshold=0.8,
+        verificator_threshold=0.9,
+        face_crop_size=(160, 160),
+    )
